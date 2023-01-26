353112449a5cc835d49dfe6de4b79aa479f54967
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2023-01-26 17:29:24 +0530

    User creation

--- authentication/views.py
+++ authentication/views.py
@@ -3,8 +3,23 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
+from django.contrib import messages
 # Create your views here.
 
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status= 400)
+
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Sorry email is taken, choose other.'}, status= 409)
+        
+        return JsonResponse({'email_valid': True})
 class UsernameValidationView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -18,7 +33,28 @@ class UsernameValidationView(View):
         
         return JsonResponse({'username_valid': True})
 
-        
+
 class RegistrationView(View):
     def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        context = {
+            'fieldValues' : request.POST
+        }
+        if not User.objects.filter(username=username):
+            if not User.objects.filter(email=email):
+                if len(password) < 6:
+                    messages.error(request, 'Password too short')
+                    return render(request, 'authentication/register.html',context)
+                user = User.objects.create_user(username=username,email=email)
+                user.set_password(password)
+                user.save()
+                messages.success(request, 'Account created successfully')
+                return render(request, 'authentication/register.html')
+
         return render(request, 'authentication/register.html')
