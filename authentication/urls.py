9edb8248aa19fc22e15c887ee83df414b6f9d3b6
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2023-01-30 00:04:18 +0530

    reset password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import RegistrationView,VerifivationView, UsernameValidationView, EmailValidationView, LoginView, LogoutView
+from .views import RegistrationView,VerifivationView, UsernameValidationView, EmailValidationView, LoginView, LogoutView, RequestPasswordResetEmail, CompletePasswordReset
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
-    path('activate/<uid>/<token>', VerifivationView.as_view(), name='activate')
+    path('activate/<uid>/<token>', VerifivationView.as_view(), name='activate'),
+    path('/request-reset-link', RequestPasswordResetEmail.as_view(), name='request-password'),
+    path('/set-new-password/<uid>/<token>', CompletePasswordReset.as_view(), name='reset-user-password'),
 ]
